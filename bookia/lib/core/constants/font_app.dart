cddec152109e42f9990674d5c2b36685eed04796
Aya osama <ayaosama26622@gmail.com>
2026-03-04 18:37:09 +0200

    bookia commit

---
+++ bookia/lib/core/constants/font_app.dart
@@ -0,0 +1,4 @@
+
+abstract class AppFont {
+  static const String dMSerif = 'DMSerif';
+}
---
+++ bookia/lib/core/constants/image_app.dart
@@ -0,0 +1,34 @@
+
+abstract class AppImage {
+  static const String logo = 'assets/image/logo.svg';
+  static const String imagewelcome = 'assets/image/image welcome.png';
+  static const String back = 'assets/image/back.svg';
+
+
+
+
+
+
+  static const String cib_apple = 'assets/icone/cib_apple.svg';
+  static const String google = 'assets/icone/google.svg';
+
+
+  
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+ 
+}
+
+
